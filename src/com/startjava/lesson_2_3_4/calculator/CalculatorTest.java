4aa29eead8077a8a65284f63007f99c2ff9a4355
Татьяна <tanya4-spi7@yandex.ru>
2019-09-10 23:34:22 +0300

    "4_1_hw"

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_2_3.calculator;
+package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
@@ -7,20 +7,24 @@ public class CalculatorTest {
         String continuation;
         Scanner scan = new Scanner(System.in);
         do {
-            System.out.print("enter the first number");
-            int number1 = scan.nextInt();
-        
-            System.out.println("enter the operation");
-            String operation = scan.next();
+            System.out.print("enter the expression");
+            String nOperationN = scan.nextLine();
+            String[] arrOfNOperationN = nOperationN.split(" ");
+            for (String a:arrOfNOperationN)
+                System.out.println(a);
+            String part1 = arrOfNOperationN[0];
+            String part2 = arrOfNOperationN[1];
+            String part3 = arrOfNOperationN[2];
+            int number1 = Integer.parseInt(part1);
+            int number2 = Integer.parseInt(part3);
+            String operation = part2;
 
-            System.out.print("enter the second number");
-            int number2 = scan.nextInt();
-        
             System.out.println("First number = " + number1);
             System.out.println("Operation = " + operation);
             System.out.println("Second number = " + number2);
+            System.out.println("expression = " + nOperationN);
 
-            Calculator  myCalc = new Calculator();
+            Calculator myCalc = new Calculator();
             myCalc.setNumber1(number1);
             myCalc.setNumber2(number2);
             myCalc.setOperation(operation);
@@ -29,14 +33,14 @@ public class CalculatorTest {
             do {
                 System.out.println("I would like continue yes/no");
                 continuation = scan.nextLine();
-              /*  if (continuation.equals("yes")) {
+                if (continuation.equals("yes")) {
                     System.out.println("continuation = " + continuation);
                     break;
                 } else if (continuation.equals("no")) {
                     break;
                 } else {
                     System.out.println("continuation = " + continuation + "  you must enter yes or no");
-                }           */
+                }
             } while ((continuation != "yes") & (continuation != "no"));
 
         } while (continuation.equals("yes"));
